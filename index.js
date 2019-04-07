fada94ddf88c67c791628a8e50377c86cfc2dbb0
disregardfiat <sjettingerjr@gmail.com>
2019-04-06 23:08:39 -0700

    enhance

--- index.js
+++ index.js
@@ -536,7 +536,7 @@ function startApp() {
             a: 0,
             u: 0
         }
-        var availible = parseInt(vests / (state.stats.prices.listed.a * (state.stats.vs + 5) * 1000)),
+        var availible = parseInt(vests / (state.stats.prices.listed.a * (state.stats.vs - 5) * 1000)),
             used = 0;
         if (json.delegatee == 'hashkings' && vests) {
             for (var i = 0; i < state.delegations.length; i++) {
@@ -604,7 +604,7 @@ function startApp() {
                     if (amount == 500 && type == 'manage') {
                         state.bal.c += amount;
                         for (var i = 0; i < state.delegations.length; i++) {
-                            if (json.delegator == state.delegation[i].delegator && state.delegation[i].availible) {
+                            if (json.delegator == state.delegations[i].delegator && state.delegations[i].availible) {
                                 state.delegation[i].availible--;
                                 state.delegation[i].used++;
                                 allowed = true

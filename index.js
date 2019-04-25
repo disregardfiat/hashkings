cffaf4878a0d04595a4775fe62d416dd237bab84
disregardfiat <sjettingerjr@gmail.com>
2019-04-25 15:14:14 -0700

    enhancesss

--- index.js
+++ index.js
@@ -622,8 +622,8 @@ processor.onOperation('delegate_vesting_shares', function(json, from) { //grab p
         break;
       }
     }
-      console.log(json.delegatee, vests,record)
-    if (!state.users[json.delegator] && json.delegatee == username && record != '') state.users[json.delegator] = {
+      console.log(json.delegator, vests,record)
+    if (!state.users[json.delegator] && json.delegatee == username) state.users[json.delegator] = {
       addrs: [],
       seeds: [],
       inv: [],

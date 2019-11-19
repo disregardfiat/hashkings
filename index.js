ae255da66d24ae90491171b5ef3e04c05c928c15
Daniel Pittman <dpdanpittman@gmail.com>
2019-11-18 18:40:20 -0800

    updated account history

--- index.js
+++ index.js
@@ -105,7 +105,7 @@ var processor;
 var recents = []
 const transactor = steemTransact(client, steem, prefix);
 
-steemjs.api.getAccountHistory(username, -1, 100, function(err, result) {
+steemjs.api.getAccountHistory(username, -1, 200, function(err, result) {
   if (err){
     console.log(err)
     startWith(sh)
